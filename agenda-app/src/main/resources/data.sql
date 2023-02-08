f22dda1e104a9b38c0eaf751b8f55b36283408de
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2023-02-08 20:32:33 -0300

    Adição de usuários pelo script sql

--- agenda-app/src/main/resources/data.sql
+++ agenda-app/src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO
 VALUES
     ("CLIENTE"),
     ("PRESTADOR");
+
+INSERT INTO
+    `tb_usuario` (`nome`, `sobrenome`, `email`, `senha`, `id_tipo_usuario`)
+VALUES
+    ("Usuário", "Cliente", "cliente@agendapp.com.br", "cliente", 1),
+    ("Usuário", "Prestador", "prestador@agendapp.com.br", "prestador", 2);
